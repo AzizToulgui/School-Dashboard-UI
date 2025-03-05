904982912c09051173ba1e245fc1fbb880dcc050
Aziz <aziztoulgui20@gmail.com>
2025-03-05 17:24:28 +0100

    begin navbar desgin

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
+import Menu from '@/components/Menu';
+import Navbar from "@/components/Navbar";
 export default function DashboardLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
@@ -7,9 +9,18 @@ export default function DashboardLayout({children,}: Readonly<{
 
         <div className="h-screen flex" >
             {/*left*/}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-100">left</div>
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+                <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block">School</span>
+                </Link>
+                <Menu/>
+            </div>
             {/*Right*/}
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-100">right</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+                <Navbar/>
+                {children}
+            </div>
         </div>
 
     );
