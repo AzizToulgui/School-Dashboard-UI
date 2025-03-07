c967990cb232dc54f21fa282ed6a92d92ce9be74
Aziz <aziztoulgui20@gmail.com>
2025-03-07 19:51:59 +0100

    add react calender

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,6 +2,7 @@ import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
+import EventCalendar from "@/components/EventCalendar";
 
 const AdminPage = () => {
     return (
@@ -33,8 +34,8 @@ const AdminPage = () => {
             </div>
             {/* RIGHT */}
             <div className="w-full lg:w-1/3 flex flex-col gap-8">
-               {/* <EventCalendar />
-                <Announcements/>*/}
+                <EventCalendar />
+                {/*<Announcements/>*/}
             </div>
         </div>
     );
