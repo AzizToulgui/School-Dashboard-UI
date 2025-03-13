6f57f2e5b4fda5f10c47e0632159e66a0924d3ba
Aziz <aziztoulgui20@gmail.com>
2025-03-13 16:16:26 +0100

    finish single teacher page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import BigCalendar from "@/components/BigCalender";
+import Announcements from "@/components/Announcements";
+import Link from "next/link";
+import Performance from "@/components/Performance";
 
 const SingleTeacherPage = () => {
     return (
@@ -36,21 +40,63 @@ const SingleTeacherPage = () => {
                         </div>
                     </div>
                     {/*Small Cards*/}
-                    <div className="flex-1 ">
-
+                    <div className="flex-1 flex gap-4 justify-between flex-wrap ">
+                        {/*Card 1*/}
+                        <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[47%] ">
+                            <Image src="/singleAttendance.png" alt="" width={24} height={24} className="w-6 h-6 "/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">90%</h1>
+                                <span className="text-sm text-gray-400">Attendance</span>
+                            </div>
+                        </div>
+                        {/*Card 2*/}
+                        <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[47%]">
+                            <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6 "/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">2</h1>
+                                <span className="text-sm text-gray-400">Branches</span>
+                            </div>
+                        </div>
+                        {/*Card 3*/}
+                        <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[47%]">
+                            <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6 "/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">6</h1>
+                                <span className="text-sm text-gray-400">Lessons</span>
+                            </div>
+                        </div>
+                        {/*Card 4*/}
+                        <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[47%]">
+                            <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6 "/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">6</h1>
+                                <span className="text-sm text-gray-400">Classes</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {/*Bottom*/}
-                <div className="">
-                    Schedule
+                <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+                    <h1 className="">Teacher&apos;s Schedule</h1>
+                    <BigCalendar/>
                 </div>
             </div>
             {/*Right*/}
-            <div className="w-full xl:w-1/3">R</div>
+            <div className="w-full xl:w-1/3 flex flex-col gap-4 ">
+                <div className="bg-white p-4 rounded-md ">
+                    <h1 className="text-xl font-semibold">Shortcuts</h1>
+                    <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500 ">
+                        <Link href="/" className="p-3 rounded-md bg-dashSkyLight">Teacher&apos;s Classes</Link>
+                        <Link href="/" className="p-3 rounded-md bg-dashPurpleLight">Teacher&apos;s Students</Link>
+                        <Link href="/" className="p-3 rounded-md bg-dashYellowLight">Teacher&apos;s Lessons</Link>
+                        <Link href="/" className="p-3 rounded-md bg-pink-50">Teacher&apos;s Exams</Link>
+                        <Link href="/" className="p-3 rounded-md bg-dashSkyLight">Teacher&apos;s Assignments</Link>
+                    </div>
+                </div>
+                <Performance/>
+                <Announcements/>
+            </div>
         </div>
     )
 }
 export default SingleTeacherPage
-
-
-                                            /*2:38:28*/
